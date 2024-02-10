2a54be67b9ab253a85c7987036e23823cab269a9
Vivek Thakur <20509.vivek@gpranchi.org>
2024-02-10 11:20:10 +0530

    commit

--- index.mjs
+++ index.mjs
@@ -1,21 +1,4 @@
-import XDEA from "./xdea.js";
-import { Client, Intents as DiscordIntents } from "discord.js";
+import XDEA from "./xdea.mjs";
 
-const bot = new XDEA("./intents.json");
-const client = new Client({
-  intents: [
-    DiscordIntents.FLAGS.GUILD_MESSAGES,
-    DiscordIntents.FLAGS.GUILD_MESSAGE_CONTENT,
-  ],
-});
-
-client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on("messageCreate", (message) => {
-  if (message.author.bot) return;
-  bot.chat(message);
-});
-
-client.login(process.env.DISCORD_TOKEN);
+const modelInstance = new XDEA("./save/intents.json");
+modelInstance.chat();
---
+++ xdea.mjs
@@ -0,0 +1,75 @@
+import { writeFileSync, readFileSync } from "fs";
+import { createInterface } from "readline";
+
+class XDEA {
+  constructor(intentFile) {
+    this.intentFile = intentFile;
+    this.Intents = JSON.parse(readFileSync(intentFile, "utf-8"));
+    this.qKeys = Object.keys(this.Intents);
+  }
+
+  mostCommon(lst) {
+    const data = lst.reduce((acc, val) => {
+      acc[val] = (acc[val] || 0) + 1;
+      return acc;
+    }, {});
+
+    if (lst.length !== 0) {
+      return Object.keys(data).reduce((a, b) => (data[a] > data[b] ? a : b));
+    }
+
+    return "I do not know.";
+  }
+
+  saveIntents() {
+    writeFileSync(this.intentFile, JSON.stringify(this.Intents, null, 2));
+  }
+
+  async chat() {
+    const rl = createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+
+    let player = "";
+
+    while (player !== "q") {
+      process.stdout.write("<user> ");
+
+      player = await new Promise((resolve) => {
+        rl.question("<user> ", resolve);
+      });
+
+      if (!this.Intents[player]) {
+        this.Intents[player] = [];
+        this.qKeys.push(player);
+      } else {
+        process.stdout.write(
+          `<xdea> ${this.mostCommon(this.Intents[player])}\n`
+        );
+      }
+
+      const question =
+        this.qKeys[Math.floor(Math.random() * this.qKeys.length)];
+      process.stdout.write(`<xdea> ${question}\n`);
+      process.stdout.write("<response learned>");
+
+      player = await new Promise((resolve) => {
+        rl.question("<user> ", resolve);
+      });
+
+      let a = this.Intents[question] || [];
+      if (!Array.isArray(a)) {
+        a = [a];
+      }
+
+      a.push(player);
+      this.Intents[question] = a;
+      this.saveIntents();
+    }
+
+    rl.close();
+  }
+}
+
+export default XDEA;
